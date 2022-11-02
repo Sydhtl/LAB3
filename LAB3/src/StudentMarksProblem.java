f691d1a6f53f3ee714e6a8d1f0e985671b3f66d8
saidahtul <saidahtul@10.106.17.163>
2022-11-03 07:07:07 +0800

    LAB3

--- LAB3/src/StudentMarksProblem.java
+++ LAB3/src/StudentMarksProblem.java
@@ -5,30 +5,30 @@ public class StudentMarksProblem {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Enter Matric ");
+		System.out.println("Enter Matric :");
 		int Matric = input.nextInt();
 		
 		
-		System.out.println("Enter Assesment1 Marks");
+		System.out.println("Enter Assesment1 Marks:");
 		double Assesment1Marks = input.nextDouble();
 		final double Assesment1Percentage= 0.15 ;
 		
-		System.out.println("Enter Assesment2 Marks");
+		System.out.println("Enter Assesment2 Marks:");
 		Double Assesment2Marks = input.nextDouble();
 		final double Assesment2Percentage = 0.20 ;
 		
 		
-		System.out.println("Enter Lab Assignment Marks");
+		System.out.println("Enter Lab Assignment Marks:");
 		Double LabAssignmentMarks = input.nextDouble();
 		final double LabAssignmentPercentage = 0.35;
 		
-		System.out.println("Enter Final Exam Marks");
+		System.out.println("Enter Final Exam Marks:");
 		double FinalExamMarks = input.nextDouble();
 		final double FinalExamPercentage = 0.30 ;
 		
-		Double OverallMarks=( Assesment1Marks * Assesment1Percentage) + (Assesment2Marks * Assesment2Percentage) + (LabAssignmentMarks * LabAssignmentPercentage)+(FinalExamMarks *FinalExamPercentage);
+		Double TotalMarks=( Assesment1Marks * Assesment1Percentage) + (Assesment2Marks * Assesment2Percentage) + (LabAssignmentMarks * LabAssignmentPercentage)+(FinalExamMarks *FinalExamPercentage);
 		
-		System.out.println("Overall Marks for  student with Matric " + Matric + " is " + OverallMarks );
+		System.out.println("Your total marks for CSC3100 (" +((Assesment1Marks * Assesment1Percentage )+ (" + ")+(Assesment2Marks * Assesment2Percentage) + (" + ")+ (LabAssignmentMarks * LabAssignmentPercentage) + (" + ")+ (FinalExamMarks *FinalExamPercentage ) +(") is ") + TotalMarks ));
 		
 		
 		
